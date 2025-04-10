9caf1b88259af1049d8b6c47a4756f47fec5c81a
Alain Daly <alain.daly.93600@gmail.com>
2025-04-10 12:42:26 +0200

    placing images on index.html

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,30 +1,40 @@
-    async function getPhotographers() {
+    async function getPhotographers(){
+        let request = await fetch("../data/photographers.json");
+        let data = await request.json()
+
+        console.log(data);
+        console.log(data.photographers); // les profils
+        console.log(data.media);         // les médias
+        
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+        // let photographers = [
+        //     {
+        //         "name": "Ma data test",
+        //         "id": 1,
+        //         "city": "Paris",
+        //         "country": "France",
+        //         "tagline": "Ceci est ma data test",
+        //         "price": 400,
+        //         "portrait": "account.png"
+        //     },
+        //     {
+        //         "name": "Autre data test",
+        //         "id": 2,
+        //         "city": "Londres",
+        //         "country": "UK",
+        //         "tagline": "Ceci est ma data test 2",
+        //         "price": 500,
+        //         "portrait": "account.png"
+        //     },
+        // ]
         // et bien retourner le tableau photographers seulement une fois récupéré
         return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+            photographers: [data.photographers, data.photographers, data.photographers, data.photographers, data.photographers, data.photographers], 
+            // media: [data.media]
+            })
     }
+    
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
@@ -32,14 +42,30 @@
         photographers.forEach((photographer) => {
             const photographerModel = photographerTemplate(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
+
+            console.log(photographer.portrait)
+            
+            userCardDOM.innerHTML = `
+            <img src="assets/photographers/Sample-photos/Photographers-ID-Photos/${photographer.portrait}" alt="${photographer.name}">
+            <h1>${photographer.name}</h1>
+            <p>${photographer.city}, ${photographer.country}</p>
+            <p>${photographer.tagline}</p>
+            <p>${photographer.price}€/jour</p>
+            `
             photographersSection.appendChild(userCardDOM);
         });
     }
 
     async function init() {
         // Récupère les datas des photographes
+        const response = await fetch("../data/photographers.json");
+        const data = await response.json();
+
+        console.log(data.photographers); // affiche un tableau de photographes
+        
+
         const { photographers } = await getPhotographers();
-        displayData(photographers);
+        displayData(data.photographers); //Le tableau s'active ici
     }
     
     init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,7 +1,7 @@
 function photographerTemplate(data) {
     const { name, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/Sample-photos/Photographers-ID-Photos/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
