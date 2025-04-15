ea78b379a2d14cbd439086d8ec9328f3072fd7b1
Alain Daly <alain.daly.93600@gmail.com>
2025-04-15 10:59:42 +0200

    refactory page/photographer.js with the fill style.css

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,37 +1,11 @@
     async function getPhotographers(){
-        let request = await fetch("../data/photographers.json");
-        let data = await request.json()
-
-        console.log(data);
-        console.log(data.photographers); // les profils
-        console.log(data.media);         // les médias
+        const request = await fetch("../data/photographers.json");
+        const data = await request.json()
         
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        // let photographers = [
-        //     {
-        //         "name": "Ma data test",
-        //         "id": 1,
-        //         "city": "Paris",
-        //         "country": "France",
-        //         "tagline": "Ceci est ma data test",
-        //         "price": 400,
-        //         "portrait": "account.png"
-        //     },
-        //     {
-        //         "name": "Autre data test",
-        //         "id": 2,
-        //         "city": "Londres",
-        //         "country": "UK",
-        //         "tagline": "Ceci est ma data test 2",
-        //         "price": 500,
-        //         "portrait": "account.png"
-        //     },
-        // ]
         // et bien retourner le tableau photographers seulement une fois récupéré
         return ({
-            photographers: [data.photographers, data.photographers, data.photographers, data.photographers, data.photographers, data.photographers], 
-            // media: [data.media]
+            photographers:data.photographers, 
+            media: data.media
             })
     }
     
@@ -43,27 +17,14 @@
             const photographerModel = photographerTemplate(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
 
-            console.log(photographer.portrait)
-            
-            userCardDOM.innerHTML = `
-            <img src="assets/photographers/Sample-photos/Photographers-ID-Photos/${photographer.portrait}" alt="${photographer.name}">
-            <h2>${photographer.name}</h2>
-            <h3>${photographer.city}, ${photographer.country}</h3>
-            <h4>${photographer.tagline}</h4>
-            <p>${photographer.price}€/jour</p>
-            `
             photographersSection.appendChild(userCardDOM);
         });
     }
 
     async function init() {
         // Récupère les datas des photographes
-        const response = await fetch("../data/photographers.json");
-        const data = await response.json();
-
-        console.log(data.photographers); // affiche un tableau de photographes
-        
-        displayData(data.photographers); //Le tableau s'active ici
+    const { photographers } = await getPhotographers();
+    displayData(photographers);
     }
     
     init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,23 +1,35 @@
 function photographerTemplate(data) {
-    const { name, portrait, id} = data;
+    const { name, portrait, id, city, country, tagline, price} = data;
+    console.log(data);
+    
 
     const picture = `assets/photographers/Sample-photos/Photographers-ID-Photos/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const a = document.createElement('a')
+        a.setAttribute("href", `http://127.0.0.1:5500/photographer.html?id=${id}`)
+        const article = document.createElement('article');
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
+        const h2Name = document.createElement('h2');
+        h2Name.textContent = name;
+        const h3Location = document.createElement('h3');
+        h3Location.textContent = `${city}, ${country}`;
+        const h4Tagline = document.createElement('h4');
+        h4Tagline.textContent = `${tagline}`;
+        const pPrice = document.createElement('p');
+        pPrice.textContent = `${price}€/jour`;
+
         article.appendChild(img);
-        article.appendChild(h2);
+        article.appendChild(h2Name);
+        article.appendChild(h3Location);
+        article.appendChild(h4Tagline);
+        article.appendChild(pPrice);
 
-        // l'événement qui permet d'allez sur la page du photographe concerné en utilisant sa data.id "?id${id}"
-        article.addEventListener("click", () => {
-            window.location.href = `http://127.0.0.1:5500/photographer.html?id=${id}`
-        })
+        // Ici on met l'article DANS le lien car a est le parent de acticle afin de click sur la page du photographe
+        a.appendChild(article);
 
-        return (article);
+        return(a);
     }
     return { name, picture, getUserCardDOM }
 }
